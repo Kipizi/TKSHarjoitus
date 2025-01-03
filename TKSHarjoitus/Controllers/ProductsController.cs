6aea6406d20b3c5c13383310927058e6cce58c25
Antti Pohjantuuli <antti.pohjantuuli@student.careeria.fi>
2025-01-03 17:06:08 +0200

    Add project files.

---
+++ TKSHarjoitus/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TKSHarjoitus.Models;
+
+
+namespace TKSHarjoitus.Controllers
+{
+    public class TuotteetController : Controller
+    {
+        // GET: Products
+        public ActionResult Tuotteet()
+        {
+            ViewBag.LoggedStatus = "Log in/out";
+            TilausDBEntities db = new TilausDBEntities();
+            List<Tuotteet> model = db.Tuotteet.ToList();
+            db.Dispose();
+            return View(model);
+        }
+        public ActionResult Tuotteet2()
+        {
+            ViewBag.LoggedStatus = "Log in/out";
+            TilausDBEntities db = new TilausDBEntities();
+            List<Tuotteet> model = db.Tuotteet.ToList();
+            db.Dispose();
+            return View(model);
+        }
+    }
+}
